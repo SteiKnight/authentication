4ea3cb237baace9a1b63853a7d9123cdaa905e46
Delan Gono <30848798+SteiKnight@users.noreply.github.com>
2025-01-25 23:24:58 +0100

    modified: setup firebase

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -15,133 +15,134 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.grey[300],
-        body: SafeArea(
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(
-                  height: 50,
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 50,
+              ),
+              //logo
+              const Icon(
+                Icons.lock,
+                size: 80,
+              ),
+
+              const SizedBox(height: 50),
+              //welcome text
+              Text(
+                'Welcome back you\'ve been missed!',
+                style: TextStyle(
+                  color: Colors.grey[700],
+                  fontSize: 16,
                 ),
-                //logo
-                const Icon(
-                  Icons.lock,
-                  size: 80,
+              ),
+
+              const SizedBox(height: 25),
+              //username
+              InputBox(
+                controller: userController,
+                obscuredText: false,
+                hintText: 'Username',
+              ),
+
+              SizedBox(height: 10),
+              //password
+              InputBox(
+                controller: passwordController,
+                obscuredText: true,
+                hintText: 'Password',
+              ),
+
+              SizedBox(height: 10),
+
+              //forgot password
+
+              Padding(
+                padding: const EdgeInsets.only(
+                  right: 25,
                 ),
-
-                const SizedBox(height: 50),
-                //welcome text
-                Text(
-                  'Welcome back you\'ve been missed!',
-                  style: TextStyle(
-                    color: Colors.grey[700],
-                    fontSize: 16,
-                  ),
-                ),
-
-                const SizedBox(height: 25),
-                //username
-                InputBox(
-                  controller: userController,
-                  obscuredText: false,
-                  hintText: 'Username',
-                ),
-
-                SizedBox(height: 10),
-                //password
-                InputBox(
-                  controller: passwordController,
-                  obscuredText: true,
-                  hintText: 'Password',
-                ),
-
-                SizedBox(height: 10),
-
-                //forgot password
-
-                Padding(
-                  padding: const EdgeInsets.only(
-                    right: 25,
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Text(
-                        'Forgot Password?',
-                        style: TextStyle(color: Colors.grey[600]),
-                      ),
-                    ],
-                  ),
-                ),
-
-                SizedBox(height: 25),
-
-                //sign in button
-                MyButton(text: 'Sign In', onTap: signUserIn),
-
-                SizedBox(height: 50),
-
-                //continue with
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25),
-                  child: Row(children: [
-                    Expanded(
-                      child: Divider(
-                        thickness: 0.5,
-                        color: Colors.grey[400],
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 10.0),
-                      child: Text(
-                        'Or continue with',
-                        style: TextStyle(color: Colors.grey[700]),
-                      ),
-                    ),
-                    Expanded(
-                      child: Divider(
-                        thickness: 0.5,
-                        color: Colors.grey[400],
-                      ),
-                    ),
-                  ]),
-                ),
-                SizedBox(
-                  height: 50,
-                ),
-                //google + apple sign in buttons
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
                   children: [
-                    AlternativeLogin(
-                        imagePath: 'lib/images/google.png', onTap: googleLogin),
-                    SizedBox(width: 10),
-                    AlternativeLogin(
-                        imagePath: 'lib/images/apple.png', onTap: appleLogin),
-                  ],
-                ),
-
-                SizedBox(height: 50),
-
-                //not a member register
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text('Not a member?'),
-                    SizedBox(width: 4),
                     Text(
-                      'Register now',
-                      style: TextStyle(
-                        color: Colors.blue,
-                        fontWeight: FontWeight.bold,
-                      ),
+                      'Forgot Password?',
+                      style: TextStyle(color: Colors.grey[600]),
                     ),
                   ],
                 ),
-              ],
-            ),
+              ),
+
+              SizedBox(height: 25),
+
+              //sign in button
+              MyButton(text: 'Sign In', onTap: signUserIn),
+
+              SizedBox(height: 50),
+
+              //continue with
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25),
+                child: Row(children: [
+                  Expanded(
+                    child: Divider(
+                      thickness: 0.5,
+                      color: Colors.grey[400],
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10.0),
+                    child: Text(
+                      'Or continue with',
+                      style: TextStyle(color: Colors.grey[700]),
+                    ),
+                  ),
+                  Expanded(
+                    child: Divider(
+                      thickness: 0.5,
+                      color: Colors.grey[400],
+                    ),
+                  ),
+                ]),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              //google + apple sign in buttons
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  AlternativeLogin(
+                      imagePath: 'lib/images/google.png', onTap: googleLogin),
+                  SizedBox(width: 10),
+                  AlternativeLogin(
+                      imagePath: 'lib/images/apple.png', onTap: appleLogin),
+                ],
+              ),
+
+              SizedBox(height: 50),
+
+              //not a member register
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Not a member?'),
+                  SizedBox(width: 4),
+                  Text(
+                    'Register now',
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
